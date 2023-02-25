bc6d288eee503d58fdf0173ca52262febc308a7c
kulichek <kulichek@icloud.com>
2023-02-25 16:55:32 +0300

    Tolower

---
+++ example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Да это же МАША!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
