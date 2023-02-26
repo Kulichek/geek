1fa4c4145f20bd3a584ee6159818a8dcd12fcb14
kulichek <kulichek@icloud.com>
2023-02-26 22:55:53 +0300

    трёхзначные

---
+++ Task7/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа,которая принимает на входтрехзначные число и на выходепоказывает последнюю цифру этого числа
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int div = 0;
+
+if(num => 100 && num < 1000)
+{ 
+    div = num % 10;
+    Console.WriteLine(div);
+}    
+else 
+{
+    Console.WriteLine("Введите трёхзначное число");
+}
+
+
