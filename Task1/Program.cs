5df4e765529001ee49ec7c7c7ec07b344d377791
kulichek <kulichek@icloud.com>
2023-02-26 20:16:23 +0300

    первая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//  программа, которая на вход принимает 2 числа и проверяет является ли первое квадратом второго.
+
+// напрмер:
+//4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine("your result is: " + result);
+Console.WriteLine("Your result is: {0}", result);
+Console.WriteLine($"Your result is {number * number}");
+
+
+
