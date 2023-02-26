c8a380589d3d47fb52884ff871acedb9f755633f
kulichek <kulichek@icloud.com>
2023-02-26 22:20:48 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа,которая на вход прирнимает одно число N , а на выходе показывает все целые числа в промежутке от N до -N
+//4->"-4,-3,-2,-1,0,1,2,3,4"
+//2->"-2,-1,0,1,2"
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = num * -1;
+
+while (count <= num) {
+    Console.WriteLine(count);
+    count = count + 1;
+}
